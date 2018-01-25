08d296404f7057c6fc42789c774837f3ac84a6dd
gbrlQuiroz <orion_fire_qog@hotmail.com>
2018-01-25 13:29:43 -0600

    aplicar estilos con ngStyle y ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { NumeroAleatorioComponent } from './numero-aleatorio/numero-aleatorio.component';
 import { SaludosComponent } from './saludos/saludos.component';
 import { OcultarComponent } from './ocultar/ocultar.component';
+import { EstilosComponent } from './estilos/estilos.component';
 
 @NgModule({
-  declarations: [AppComponent, NumeroAleatorioComponent, SaludosComponent, OcultarComponent],
+  declarations: [AppComponent, NumeroAleatorioComponent, SaludosComponent, OcultarComponent, EstilosComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
