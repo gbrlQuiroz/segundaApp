f3ae2a4220de106a56ec658e499aeb42b01c761c
gbrlQuiroz <orion_fire_qog@hotmail.com>
2018-01-31 15:23:22 -0600

    implementando routing y bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NumeroAleatorioComponent } from './numero-aleatorio/numero-aleatorio.component';
+import { SaludosComponent } from './saludos/saludos.component';
+import { OcultarComponent } from './ocultar/ocultar.component';
+import { EstilosComponent } from './estilos/estilos.component';
+import { ListasComponent } from './listas/listas.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { Error404Component } from './error404/error404.component';
+
+const routes: Routes = [
+  {
+    path: 'aleatorio',
+    component: NumeroAleatorioComponent
+  },
+  {
+    path: 'saludos',
+    component: SaludosComponent
+  },
+  {
+    path: 'ocultar',
+    component: OcultarComponent
+  },
+  {
+    path: 'estilos',
+    component: EstilosComponent
+  },
+  {
+    path: 'listas',
+    component: ListasComponent
+  },
+  {
+    path: 'error404',
+    component: Error404Component
+  },
+  {
+    path: '',
+    component: AcercaDeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'error404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [
+  NumeroAleatorioComponent,
+  SaludosComponent,
+  OcultarComponent,
+  EstilosComponent,
+  ListasComponent,
+  AcercaDeComponent,
+  Error404Component
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // para poder utilizar [(ngModel)]
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+/*import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 import { NumeroAleatorioComponent } from './numero-aleatorio/numero-aleatorio.component';
 import { SaludosComponent } from './saludos/saludos.component';
 import { OcultarComponent } from './ocultar/ocultar.component';
 import { EstilosComponent } from './estilos/estilos.component';
-import { ListasComponent } from './listas/listas.component';
+import { ListasComponent } from './listas/listas.component'; */
 
 @NgModule({
-  declarations: [AppComponent, NumeroAleatorioComponent, SaludosComponent, OcultarComponent, EstilosComponent, ListasComponent],
+  /* declarations: [AppComponent, NumeroAleatorioComponent, SaludosComponent, OcultarComponent, EstilosComponent, ListasComponent], */
+  declarations: [AppComponent, routingComponents],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
